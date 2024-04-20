9a49266e29ae39c4e09608cb79e3938824c5ad16
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-20 16:53:29 +0530

    added bank account schema

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Schema } = require("zod");
 
 mongoose.connect(
   "mongodb+srv://vanshajsharma:oGMhAdX46KpdZ3li@cluster0.downyiq.mongodb.net/paytm_vanz_app"
@@ -33,8 +34,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
   User,
+  Account,
 };
