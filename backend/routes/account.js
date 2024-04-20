733514fc0a6c166d7e81a668311c1ceb56d48c4f
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-20 17:04:04 +0530

    added balance route in account

---
+++ backend/routes/account.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { authMiddleware } = require("../middleware");
+const { Account } = require("../db");
+const router = express.Router();
+
+router.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+
+  res.json({
+    balance: account.balance,
+  });
+});
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userRoute = require("./user");
+const accountRoute = require("./account.js");
 
 router.use("/user", userRoute);
+router.use("/account", accountRoute);
 
 module.exports = router;
