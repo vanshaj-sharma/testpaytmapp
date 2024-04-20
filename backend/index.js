f44c7da02c0cebec01e3038a1b99da3856fa31f2
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-20 15:29:42 +0530

    added cors, body parser and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
-
+var cors = require("cors");
 const app = express();
+const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/user", rootRouter);
+
+app.listen(PORT, () => {
+  console.log("Backent currently active at " + PORT);
+});
