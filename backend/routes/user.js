83fec229bf591431a99a6b4596237e3719dab35c
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-20 15:57:25 +0530

    added signin route (unchecked)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,7 +9,7 @@ const signupBody = zod.object({
   username: zod.string().email(),
   firstName: zod.string(),
   lastName: zod.string(),
-  password: zod.string.min(6),
+  password: zod.string(),
 });
 router.post("/signup", async (req, res) => {
   const { success } = zod.safeParse(req.body);
@@ -43,6 +43,34 @@ router.post("/signup", async (req, res) => {
   res.json({
     message: "User created successfully",
     token: token,
+  });
+});
+
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+router.post("/signin", async (req, res) => {
+  const { success } = zod.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({ message: "Incorrect inputs" });
+  }
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    res.json({
+      token: token,
+    });
+
+    return;
+  }
+  res.status(411).json({
+    message: "Error while logging in",
   });
 });
 
