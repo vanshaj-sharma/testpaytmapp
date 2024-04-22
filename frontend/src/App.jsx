5dd246b0faffb0c1550cda0445fc80647c48ef21
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-22 12:26:39 +0530

    added routes to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
 function App() {
-
   return (
     <div>
-        Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element=<Signup /> />
+          <Route path="/signin" element=<Signin /> />
+          <Route path="/Dashboard" element=<Dashboard /> />
+          <Route path="/send" element=<SendMoney /> />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
