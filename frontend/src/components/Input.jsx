413b53c1a1a38cdc97f6c832ccece9a257d65d5d
vanshaj-sharma <sharmavanshaj2020@gmail.com>
2024-04-22 12:39:27 +0530

    added reuseable components and signup page

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Input = ({ label, placeholder, onChange }) => {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">{label}</div>
+      <input
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full px-2 py-1 border rounded border-slate-200"
+      />
+    </div>
+  );
+};
+
+export default Input;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import Input from "../components/Input";
 
-const Signup = () => {
-  return <div>Signup</div>;
+import BottomWarning from "../components/BottomWarning";
+import Button from "../components/Button";
+
+export const Signup = () => {
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter your infromation to create an account"} />
+          <Input placeholder="John" label={"First Name"} />
+          <Input placeholder="Doe" label={"Last Name"} />
+          <Input placeholder="harkirat@gmail.com" label={"Email"} />
+          <Input placeholder="123456" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign up"} />
+          </div>
+          <BottomWarning
+            label={"Already have an account?"}
+            buttonText={"Sign in"}
+            to={"/signin"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
